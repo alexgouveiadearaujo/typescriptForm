a5c520d3e543bc459f40d902a4f88c378ef874c0
Alex <alexgouveiadearaujo@gmail.com>
2021-12-06 19:51:27 -0300

    creating updateView method

--- dist/js/models/negotiation.js
+++ dist/js/models/negotiation.js
@@ -12,15 +12,3 @@ export class Negotiation {
         return this.amount * this.value;
     }
 }
-/*
-export class Negotiation {
-    private _date: Date;
-    private _amount: number;
-   private _value: number;
-
-  constructor(date: Date,amount: number,value: number) {
-    this._date = date;
-    this._amount = amount;
-    this._value = value;
-  }
-*/
