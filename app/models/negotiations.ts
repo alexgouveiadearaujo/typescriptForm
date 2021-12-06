c763fa6ffd5d03939bcb472acd01958d62c72e09
Alex <alexgouveiadearaujo@gmail.com>
2021-12-06 19:45:15 -0300

    adding method type

--- app/models/negotiations.ts
+++ app/models/negotiations.ts
@@ -3,11 +3,11 @@ import { Negotiation } from "./negotiation.js";
 export class Negotiations {
   private negotiations: Negotiation[] = [];
 
-  addition(negotiation: Negotiation) {
+  public addition(negotiation: Negotiation) {
     this.negotiations.push(negotiation);
   }
 
-  list(): readonly Negotiation[] {
+  public list(): readonly Negotiation[] {
     return this.negotiations;
   }
 }
