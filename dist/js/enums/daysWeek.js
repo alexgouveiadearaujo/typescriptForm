56d6944467ee2da5273bfc85e8ef77baf50d98dd
Alex <alexgouveiadearaujo@gmail.com>
2021-12-06 21:20:08 -0300

    creating enum days week

---
+++ dist/js/enums/daysWeek.js
@@ -0,0 +1,10 @@
+export var DaysWeek;
+(function (DaysWeek) {
+    DaysWeek[DaysWeek["SUNDAY"] = 0] = "SUNDAY";
+    DaysWeek[DaysWeek["MONDAY"] = 1] = "MONDAY";
+    DaysWeek[DaysWeek["TUESDAY"] = 2] = "TUESDAY";
+    DaysWeek[DaysWeek["WEDNESDAY"] = 3] = "WEDNESDAY";
+    DaysWeek[DaysWeek["THURSDAY"] = 4] = "THURSDAY";
+    DaysWeek[DaysWeek["FRIDAY"] = 5] = "FRIDAY";
+    DaysWeek[DaysWeek["SARTUDAY"] = 6] = "SARTUDAY";
+})(DaysWeek || (DaysWeek = {}));
