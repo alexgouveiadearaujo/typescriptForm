82c059c02679e6a30f80388f5b68134e229c9ee2
Alex <alexgouveiadearaujo@gmail.com>
2021-12-08 18:29:46 -0300

    add event to btn-import

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -10,3 +10,12 @@ if (form) {
 else {
     throw Error("Não foi possível inicializar aplicação.");
 }
+const btnImport = document.querySelector("#btn-import");
+if (btnImport) {
+    btnImport.addEventListener("click", () => {
+        controller.importData();
+    });
+}
+else {
+    throw Error("Botão Importa, não foi encontrado.");
+}
